6960851db6fb74d21f9a7348b5e09b6ca2c780c1
Abdulmannan Khan <abdulmannan.khan@kaust.edu.sa>
2022-10-18 15:19:49 +0300

    Add project files.

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=UserMicroservice;Integrated Security=true;MultipleActiveResultSets=True");
+        }
+    }
+}
